868c123cf74263d86d375b89a1a1137a1ec516c0
Martijn Gonlag <martijngonlag@gmail.com>
2013-04-23 15:23:36 -0700

    Changed printed output

--- d_attack.py
+++ d_attack.py
@@ -3,24 +3,26 @@ from hashlib import md5
 import sys
 
 if len(sys.argv) != 3:
-	print "Usage: %s <file of hashes> <word list>" % sys.argv[0]
-	quit()
+    print "Usage: %s <file of hashes> <word list>" % sys.argv[0]
+    quit()
 
 def bruteforce(hash_list, word_list):
-	cracked_hashes = {}
-	hashes = [line.strip() for line in open(hash_list, 'r')]
+    cracked_hashes = {}
+    hashes = [line.strip() for line in open(hash_list, 'r')]
 
-	for word in open(word_list, 'r'):
-		word = word.rstrip()
-		hash = md5(word).hexdigest()
-		
-		if hash in hashes:
-			cracked_hashes[hash] = word
-			hashes.remove(hash)
-			if hashes == []:
-				break
-				
-	return cracked_hashes			
-	
+    for word in open(word_list, 'r'):
+        word = word.rstrip()
+        hash = md5(word).hexdigest()
+        
+        if hash in hashes:
+            cracked_hashes[hash] = word
+            hashes.remove(hash)
+            if hashes == []:
+                break
+
+    return cracked_hashes
+    
 cracked_hashes = bruteforce(sys.argv[1], sys.argv[2])
-print cracked_hashes
+
+for hash in cracked_hashes:
+	print "Found: %s as %s" % (hash, cracked_hashes[hash])
